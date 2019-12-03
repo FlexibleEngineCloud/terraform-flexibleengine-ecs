cb323f80e7ba9601c7d3c7dc9862004a9914120e
Valentin LANDEMAINE <valentin.landemaine.ext@orange.com>
2019-12-03 11:50:28 +0100

    Fix outputs variables (Array in array to simple array)

--- outputs.tf
+++ outputs.tf
@@ -6,26 +6,26 @@ locals {
 
 output "private_ip" {
   description = "List of ipv4 addresses of the created servers"
-  value       = [flexibleengine_compute_instance_v2.instances.*.access_ip_v4]
+  value       = flexibleengine_compute_instance_v2.instances.*.access_ip_v4
 }
 
 output "public_ip" {
   description = "List of public floating ip addresses of the created servers"
-  value       = [flexibleengine_networking_floatingip_v2.fip.*.address]
+  value       = flexibleengine_networking_floatingip_v2.fip.*.address
 }
 
 output "neutron_ports_id" {
   description = "List of neutron ports of the created servers"
-  value       = [flexibleengine_networking_port_v2.instance_port.*.id]
+  value       = flexibleengine_networking_port_v2.instance_port.*.id
 }
 
 output "id" {
   description = "list of IDs of the created servers"
-  value       = [local.this_id]
+  value       = local.this_id
 }
 
 output "name" {
   description = "list of names of the created servers"
-  value       = [flexibleengine_compute_instance_v2.instances.*.name]
+  value       = flexibleengine_compute_instance_v2.instances.*.name
 }
 
