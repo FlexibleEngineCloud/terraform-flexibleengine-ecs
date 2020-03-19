62666b00ea3ef626785f1b0ffeb67b5161aaa151
olivier <olivier.saluden@orange.com>
2020-03-19 19:02:15 +0100

    Add ECS with new EIP example

---
+++ examples/basic-ecs-with-new-eip/main.tf
@@ -0,0 +1,32 @@
+module "basic_ecs" {
+  source = "../../"
+
+  instance_name     = "ecs-basic"
+  instance_count    = 1
+  availability_zone = "eu-west-0a"
+
+  flavor_name     = "t2.small"
+  key_name        = var.key_pair
+  security_groups = var.security_group_id
+  subnet_id       = var.subnet_id
+  network_id      = var.network_id
+
+  new_eip = true
+  eip_bandwidth = var.eip_bandwidth
+
+  block_devices = [
+    {
+      uuid                  = "<ImageID>"
+      source_type           = "image"
+      destination_type      = "volume"
+      volume_size           = 50
+      boot_index            = 0
+      delete_on_termination = true
+    }
+  ]
+
+  metadata = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
---
+++ examples/basic-ecs-with-new-eip/parameters.tfvars
@@ -0,0 +1,5 @@
+key_pair          = "<FE_KEY_PAIR_NAME>"
+network_id        = "<FE_NETWORK_ID>"
+subnet_id         = "<FE_SUBNET_ID>"
+security_group_id = "<FE_SECURITY_GROUP_ID>"
+eip_bandwidth = "8"
