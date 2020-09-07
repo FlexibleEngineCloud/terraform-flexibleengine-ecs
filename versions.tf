22a6088f0a620e467a680519dd8eb38d96578a06
Antonin <antonin.anchisi@orange.com>
2020-09-07 17:29:16 +0200

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     flexibleengine = {
-      source = "terraform-providers/flexibleengine"
+      source = "FlexibleEngineCloud/flexibleengine"
     }
   }
   required_version = ">= 0.13"
